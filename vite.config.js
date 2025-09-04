d0eafb136f382b9283dcfc372448e17934e6df05
Irina Semakova <Semakovais@yandex.ru>
2025-09-04 19:47:44 +0300

    second commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ghPages } from 'vite-plugin-gh-pages'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
-  }
+  plugins: [react(), ghPages()],
+  base: '/portfolio-filter/'
 })
